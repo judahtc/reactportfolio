a57700578b3805f60f7e29a673cbc26ca57d1345
judahtc <jaycea247@gmail.com>
2025-01-22 16:11:08 +0200

    social medial icons

--- portfolio/src/components/HeroSection.tsx
+++ portfolio/src/components/HeroSection.tsx
@@ -10,7 +10,7 @@ export default function HeroSection(){
         <p className="mt-7"> 🚀 Majoring in Enterprise Systems Development(FastAPI+ (React/Angular))</p>
         <p className="mt-2"> ⚡ Senior Software Engineer at <a className="text-blue-600" href="https://claxonactuaries.com/" target="_blank">Claxon Actuaries</a>.</p>
         
-        <div>
+        <div className=" flex items-center space-x-2  mt-4">
             <div className="github">
             <a href="" className="flex items-center space-x-2">
             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 96 96">
@@ -23,14 +23,18 @@ export default function HeroSection(){
                 </div>
             <div className="linkedin"><a href="" className="flex items-center space-x-2"> 
                 
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>
+
                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 48 48">
 <path fill="#0288D1" d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z"></path><path fill="#FFF" d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z"></path>
 </svg>
                 
                 
                 <span>LinkedIn</span></a></div>
-            <div className="email"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-mail"><rect width="20" height="16" x="2" y="4" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg> Email</div>
+            <div className="email"> <a href=""  className="flex items-center space-x-2"> 
+                
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-mail"><rect width="20" height="16" x="2" y="4" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg> 
+            
+            <span>Email</span></a></div>
         </div>
         
         </div>
