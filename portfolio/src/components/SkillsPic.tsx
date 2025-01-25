0cb82fd2cb4baba47960fbf8e8f7ab07111862e5
judahtc <jaycea247@gmail.com>
2025-01-25 17:04:57 +0200

    added animate from right of myskills pic

--- portfolio/src/components/SkillsPic.tsx
+++ portfolio/src/components/SkillsPic.tsx
@@ -1,7 +1,13 @@
 export default function SkillsPic() {
-    return <>
-    <div className="mt-24 mr-32">
-    <img src="\src\assets\images\skills.png" alt="Skills" className="oval-image" />
-    </div>
-    </>;
+    return (
+        <>
+            <div className="mt-24 mr-32 myself">
+                <img
+                    src="\src\assets\images\skills.png"
+                    alt="Skills"
+                    className="oval-image"
+                />
+            </div>
+        </>
+    );
 }
