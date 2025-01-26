8a983e9709a4916e8e6925161c8ba667881fc4b5
judahtc <jaycea247@gmail.com>
2025-01-26 22:14:46 +0200

    added animation to the articles

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import "./App.css";
 import About from "./components/About";
 import Articles from "./components/Articles";
@@ -10,12 +11,35 @@ import HeroSection from "./components/HeroSection";
 import MovingSkills from "./components/MovingSkills";
 import MyPic from "./components/MyPic";
 import NavBar from "./components/NavBar";
-import Skills from "./components/Skills";
+
 import SkillsPic from "./components/SkillsPic";
 
 import Work from "./components/Work";
 
 function App() {
+    useEffect(() => {
+        const animatedTextOne = document.getElementById("one");
+
+        if (animatedTextOne) {
+            const observer = new IntersectionObserver((entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        animatedTextOne.classList.add("animate-fade-in");
+                        observer.disconnect();
+                    }
+                });
+            });
+
+            // Start observing the element
+            observer.observe(animatedTextOne);
+
+            // Cleanup observer when component is unmounted
+            return () => {
+                observer.disconnect();
+            };
+        }
+    }, []); // Empty dependency array ensures this effect runs only once
+
     return (
         <>
             <div className="fixed w-full z-50">
--- portfolio/src/components/Articles.tsx
+++ portfolio/src/components/Articles.tsx
@@ -1,64 +1,118 @@
-export default function Articles(){
-    return(
-        <> 
-<div className="mx-40 mt-12 ">
+import { useEffect } from "react";
 
-    {/* <div className="text-center text-5xl font-semibold my-24 mt-32">📖 Blogs</div> */}
+export default function Articles() {
+    useEffect(() => {
+        const animatedTextOne = document.getElementById("one");
 
-    <div className="flex items-center justify-between my-5"><span className="text-xl font-bold">📰 Articles</span> <a target="_blank" href="https://awsfordevelopers.hashnode.dev/" className="text-green-500 text-base">View all articles →</a></div>
+        if (animatedTextOne) {
+            const observer = new IntersectionObserver((entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        animatedTextOne.classList.add("animate-fade-in");
+                        observer.disconnect();
+                    }
+                });
+            });
 
-        <div className="flex mb-10 space-x-7 text-gray-600">    
+            // Start observing the element
+            observer.observe(animatedTextOne);
 
-                <div className=" w-2/3 ">
-            <div className="articles ">
-                <div className="one  border border-gray-200  py-4  px-4  rounded-lg flex flex-col items-center" >
+            // Cleanup observer when component is unmounted
+            return () => {
+                observer.disconnect();
+            };
+        }
+    }, []); // Empty dependency array ensures this effect runs only once
 
-                    <div className=""><img  src="\src\assets\images\nodejs-lambda3.png"></img></div>
-                <div className="bg-gray-50 px-4 py-4 rounded-md"><p className="font-semibold text-xl " style={{color:"##222222"}}>A Guide: Deploying your Node.Js Application on Lambda Function using Docker</p>
-           
-                <p>
+    return (
+        <>
+            <div id="one" className="mx-40 mt-12 ">
+                {/* <div className="text-center text-5xl font-semibold my-24 mt-32">📖 Blogs</div> */}
 
-Nov 21, 2024
-·
-<span className="test-sm text-yellow-400">3 min read</span></p>
+                <div className="flex items-center justify-between my-5">
+                    <span className="text-xl font-bold">📰 Articles</span>{" "}
+                    <a
+                        target="_blank"
+                        href="https://awsfordevelopers.hashnode.dev/"
+                        className="text-green-500 text-base"
+                    >
+                        View all articles →
+                    </a>
+                </div>
 
-</div>
+                <div className="flex mb-10 space-x-7 text-gray-600">
+                    <div className=" w-2/3 ">
+                        <div className="articles ">
+                            <div className="one  border border-gray-200  py-4  px-4  rounded-lg flex flex-col items-center">
+                                <div className="">
+                                    <img src="\src\assets\images\nodejs-lambda3.png"></img>
+                                </div>
+                                <div className="bg-gray-50 px-4 py-4 rounded-md">
+                                    <p
+                                        className="font-semibold text-xl "
+                                        style={{ color: "##222222" }}
+                                    >
+                                        A Guide: Deploying your Node.Js
+                                        Application on Lambda Function using
+                                        Docker
+                                    </p>
+
+                                    <p>
+                                        Nov 21, 2024 ·
+                                        <span className="test-sm text-yellow-400">
+                                            3 min read
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col items-start space-y-3">
+                        <div className="">
+                            <div className="articles">
+                                <div className="one  border border-gray-200  py-8  px-8 bg-gray-50 rounded-lg">
+                                    <p
+                                        className="font-semibold text-lg"
+                                        style={{ color: "##222222" }}
+                                    >
+                                        A Guide: Deploying your first FastAPI
+                                        application on Amazon EC2
+                                    </p>
+                                    <br></br>
+                                    <p>
+                                        Oct 28, 2024 ·
+                                        <span className="test-sm text-yellow-400">
+                                            3 min read
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className=" ">
+                            <div className="articles">
+                                <div className="one  border border-gray-200  py-8  px-8 bg-gray-50 rounded-lg">
+                                    <p
+                                        className="font-semibold text-lg"
+                                        style={{ color: "##222222" }}
+                                    >
+                                        {" "}
+                                        A step to step guide: Hosting your
+                                        frontend application in Amazon S3
+                                    </p>
+
+                                    <p>
+                                        Sep 25, 2024 ·
+                                        <span className="test-sm text-yellow-400">
+                                            3 min read
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-
-        <div className="flex flex-col items-start space-y-3">
-        <div className="">
-            <div className="articles">
-                <div className="one  border border-gray-200  py-8  px-8 bg-gray-50 rounded-lg" >
-                <p className="font-semibold text-lg" style={{color:"##222222"}}>A Guide: Deploying your first FastAPI application on Amazon EC2</p>
-                <br></br>
-                <p>
-
-Oct 28, 2024
-·
-<span className="test-sm text-yellow-400">3 min read</span></p>
-                </div>
-            </div>
-        </div>
-        <div className=" ">
-            <div className="articles">
-                <div className="one  border border-gray-200  py-8  px-8 bg-gray-50 rounded-lg" >
-                <p className="font-semibold text-lg" style={{color:"##222222"}}> A step to step guide: Hosting your frontend application in Amazon S3</p>
-                
-                <p>
-
-Sep 25, 2024
-·
-<span className="test-sm text-yellow-400">3 min read</span></p>
-                </div>
-            </div>
-        </div>
-
-        </div>
-        </div>
-</div>
         </>
-
-    )
+    );
 }
