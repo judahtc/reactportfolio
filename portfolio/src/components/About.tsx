59b7e966a934910896c91fb52301b279bf6fa8d4
judahtc <jaycea247@gmail.com>
2025-01-30 09:12:09 +0200

    added /portfolio asa relative path too

--- portfolio/src/components/About.tsx
+++ portfolio/src/components/About.tsx
@@ -47,7 +47,7 @@ export default function About() {
                 <div className="title lg:hidden  font-bold text-lg lg:ml-3 md:ml-3  text-gray-700  lg:font-extrabold lg:text-2xl mb-5 lg:text-white md:font-extrabold md:text-2xl  md:text-white">
                     ⚡About Me
                 </div>
-                <div className="lg:w-11/12 lg:ml-3 lg:mb-3 lg:text-base w-full ml-3 mb-3 text-[0.93rem] text-gray-700">
+                <div className="lg:w-11/12 lg:ml-3 lg:mb-3 lg:text-base w-full ml-3 mb-3 text-[0.99rem] text-gray-700">
                     <p className="first mb-3">
                         I am a passionate software Engineer experienced in
                         architecturing, designing and developing Enterprice Web
@@ -98,7 +98,7 @@ export default function About() {
                             </span>
                         )}
                     </p>
-                    <div className="flex justify-center items-center mt-5 md:hidden lg:hidden">
+                    <div className="flex justify-center items-center mt-10 md:hidden lg:hidden">
                         <div className="mx-auto ">
                             <img
                                 src={judahimage2}
