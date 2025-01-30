8fdde69599f3eed6d01be89bf91a9908efbb2080
judahtc <jaycea247@gmail.com>
2025-01-30 08:50:15 +0200

    increased the height of the toggled side bar

--- portfolio/src/components/About.tsx
+++ portfolio/src/components/About.tsx
@@ -44,7 +44,7 @@ export default function About() {
                 id="animatedText"
                 style={localstyle}
             >
-                <div className="title  font-bold text-lg lg:ml-3 md:ml-3  text-gray-700  lg:font-extrabold lg:text-2xl mb-5 lg:text-white md:font-extrabold md:text-2xl  md:text-white">
+                <div className="title lg:hidden  font-bold text-lg lg:ml-3 md:ml-3  text-gray-700  lg:font-extrabold lg:text-2xl mb-5 lg:text-white md:font-extrabold md:text-2xl  md:text-white">
                     ⚡About Me
                 </div>
                 <div className="lg:w-11/12 lg:ml-3 lg:mb-3 lg:text-base w-full ml-3 mb-3 text-sm text-gray-700">
